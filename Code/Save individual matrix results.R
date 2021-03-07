5d874746cc9624a487266b43b4e030e4cbf672fe
abbylewis <aslewis@vt.edu>
2021-03-07 14:03:21 -0500

    Cleaning up github file structure

--- Code/Save individual matrix results.R
+++ Code/Save individual matrix results.R
@@ -7,7 +7,7 @@ for(i in 1:nrow(files)){
   matrix <- data.frame(t(read_sheet(files$id[i], col_types = "c")))
   colnames(matrix)<-matrix[1,]
   matrix <- matrix[2:nrow(matrix),]
-  write.csv(matrix, paste0("Finished matrices/",files$name[i], ".csv"))
+  write.csv(matrix, paste0("../Data/Finished matrices/",files$name[i], ".csv"))
 }
 files = files%>% #In case it times out, get the last few
   map_df(rev)
@@ -15,6 +15,6 @@ for(i in 1:nrow(files)){
   matrix <- data.frame(t(read_sheet(files$id[i], col_types = "c")))
   colnames(matrix)<-matrix[1,]
   matrix <- matrix[2:nrow(matrix),]
-  write.csv(matrix, paste0("Finished matrices/",files$name[i], ".csv"))
+  write.csv(matrix, paste0("../Data/Finished matrices/",files$name[i], ".csv"))
 }
 
